ca03656e740eb58e69338b0a53bed93946e97861
Nayabsaleem <nayabsaleem280@gmail.com>
2024-03-16 23:35:18 +0500

    completed my website

---
+++ logic.js
@@ -0,0 +1,12 @@
+function convertToNewPage(url) {
+                  window.location.href = url;
+}
+
+
+    document.getElementById('orderButton').addEventListener('click', function() {
+        document.getElementById('popup').style.display = 'block';
+    });
+
+    function closePopup() {
+        document.getElementById('popup').style.display = 'none';
+    }
